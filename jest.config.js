610d7315800b732f21d0b56450b744111cafc4f8
Lindemberg Nunes de Castro <lindembergncastro@outlook.com>
2022-03-12 09:54:30 -0300

    chore: add and setup jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "<rootDir>/src"
   ],
   coverageDirectory: "coverage",
+  coverageProvider: "v8",
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   testEnvironment: "node",
   transform: {
