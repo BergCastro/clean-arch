ed3d103322d78498ba3f491d41f5e8007110ede2
Lindemberg Nunes de Castro <lindembergncastro@outlook.com>
2022-03-12 18:53:17 -0300

    chore: setup huscky

--- src/usescases/registerUserOnMailingList/resgisterUserOnMaillingList.spec.ts
+++ src/usescases/registerUserOnMailingList/resgisterUserOnMaillingList.spec.ts
@@ -1,21 +1,15 @@
-import { UserData } from './UserData';
+// import { UserData } from './UserData';
 
 describe('Register User on Mailing List', () => {
-    test('should add user with complete data do mailing list', async () => {
-        const users: UserData[] = [];
-        const repo: UserData[] = InMemoryUserRepository(users);
-        // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
-        // const name = 'any_name';
-        // const email = 'any@email.com';
-        // const response = await usecase.registerUserOnMailingList({ name, email });
-        // const user = await repo.findUserByEmail(email);
+  test('should add user with complete data do mailing list', async () => {
+    // const users: UserData[] = [];
+    // const repo: UserData[] = InMemoryUserRepository(users);
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
+    // const name = 'any_name';
+    // const email = 'any@email.com';
+    // const response = await usecase.registerUserOnMailingList({ name, email });
+    // const user = await repo.findUserByEmail(email);
 
-        // expect(user.name).toBe(name);
-        
-    });  
-
-})
-
- 
-
-
+    // expect(user.name).toBe(name);
+  });
+});
