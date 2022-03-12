fc3b7a2ac49d03b6d244feac1b0d60f59a2c7301
Lindemberg Nunes de Castro <lindembergncastro@outlook.com>
2022-03-12 18:25:44 -0300

    feat: add userdatr

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
    roots: [
     "<rootDir>/src"
   ],
+  clearMocks: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
