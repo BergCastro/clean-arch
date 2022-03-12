e406bb723a30d58f765bc8af8a42a20834187153
Lindemberg Nunes de Castro <lindembergncastro@outlook.com>
2022-03-12 14:27:16 -0300

    feat: $msg

---
+++ src/usescases/registerUserOnMailingList/resgisterUserOnMaillingList.spec.ts
@@ -0,0 +1,17 @@
+import { UserData } from './UserData';
+
+describe('Register User on Mailing List', () => {
+    test('should add user with complete data do mailing list', async () => {
+        const users: UserData[] = [];
+        const repo: UserData[] = InMemoryUserRepository(users);
+        const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
+        const name = 'any_name';
+        const email = 'any@email.com';
+        const response = await usecase.registerUserOnMailingList({ name, email });
+        const user = await repo.findUserByEmail(email);
+
+        expect(user.name).toBe(name);
+      
+    });
+
+});
